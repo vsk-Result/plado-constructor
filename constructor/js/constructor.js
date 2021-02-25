16fc84919f2ff858f90d15638d1dba9ff820251e
result <result007@yandex.ru>
2021-02-26 01:14:06 +0300

    fix

--- constructor/js/constructor.js
+++ constructor/js/constructor.js
@@ -6,3 +6,24 @@ let editable = pladoConstructor({
         'Authorization': 'Bearer your-token'
     },
 })
+
+const security = document.createElement('div')
+security.classList.add('security')
+
+const securityPassword = document.createElement('input')
+securityPassword.type = 'password'
+securityPassword.placeholder = 'Введите пароль'
+security.append(securityPassword)
+
+document.body.append(security)
+securityPassword.focus()
+
+securityPassword.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        if (e.target.value === 'dashaiscool') {
+            security.remove()
+        } else {
+            e.target.value = ''
+        }
+    }
+})
