6c042d881028fe14e03027429e5d197e50ae0882
result <result007@yandex.ru>
2021-02-27 00:05:56 +0300

    fix

--- constructor/php/add_image.php
+++ constructor/php/add_image.php
@@ -8,18 +8,11 @@ function response() {
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if (!empty($_FILES)) {
-
-        ini_set('error_reporting', E_ALL);
-        ini_set('display_errors', 1);
-        ini_set('display_startup_errors', 1);
-
         foreach ($_FILES['inputfile']['name'] as $index => $filename) {
             $destinationDir = '../../images/' . $filename;
             move_uploaded_file($_FILES['inputfile']['tmp_name'][$index], $destinationDir);
         }
     }
-
-    die();
 
     response();
 }
