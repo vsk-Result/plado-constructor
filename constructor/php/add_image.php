bbc8c4434b06325d10917781f2c4100f581f4f9c
result <result007@yandex.ru>
2021-02-26 23:47:10 +0300

    fix

--- constructor/php/add_image.php
+++ constructor/php/add_image.php
@@ -7,18 +7,12 @@ function response() {
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    $request = file_get_contents('php://input');
-
-    try {
-        $payload = json_decode($request, true);
-    } catch (\Exception $e) {
-        response('Invalid JSON payload', 422);
-    }
-
-    if (isset($_FILES)) {
+    if (!empty($_FILES)) {
+        var_dump($_FILES);
+        exit;
         foreach ($_FILES['inputfile']['name'] as $index => $filename) {
-            $destiationDir = '../../images/' . $filename;
-            move_uploaded_file($_FILES['inputfile']['tmp_name'][$index], $destiationDir);
+            $destinationDir = '../../images/' . $filename;
+            move_uploaded_file($_FILES['inputfile']['tmp_name'][$index], $destinationDir);
         }
     }
 
